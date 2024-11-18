e4fdf0ecf76d678c80f5086b93a7ccc6fa2b6f78
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2024-11-18 17:15:28 +0545

    Use body-parser

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import express from 'express';
+import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import { engine } from 'express-handlebars';
 
@@ -17,8 +18,8 @@ app.use(cookieParser());
 app.use(cors());
 app.use(helmet());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 

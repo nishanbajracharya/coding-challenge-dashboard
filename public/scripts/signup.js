771edf14cbd7a8735cc7e3d6cb3870f1a5dc44c2
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2024-11-19 11:55:27 +0545

    Add passcode for signup

---
+++ public/scripts/signup.js
@@ -0,0 +1,30 @@
+const signupBtn = document.getElementById('signup-btn');
+const signupForm = document.getElementById('signup-form');
+const signupError = document.getElementById('signup-error');
+
+signupForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById('signup-email').value;
+  const password = document.getElementById('signup-password').value;
+  const passcode = document.getElementById('signup-passcode').value;
+
+  signupError.classList.add('is-hidden');
+  signupBtn.classList.add('is-loading');
+
+  try {
+    await axios.post('/api/users/signup', {
+      email, password, passcode
+    });
+    // Redirect to home
+    window.location.href = '/';
+  } catch (e) {
+    signupError.classList.remove('is-hidden');
+    
+    if (e.message) {
+      signupError.innerText = e.message;
+    }
+  } finally {
+    signupBtn.classList.remove('is-loading');
+  }
+});

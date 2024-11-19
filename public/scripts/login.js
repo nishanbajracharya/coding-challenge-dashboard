76292a990f23742aecfcd6b247c023f88fe92573
nishanbajracharya <nishanbajracharya@lftechnology.com>
2024-11-19 09:40:50 +0545

    Switch to bulma

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -8,9 +8,8 @@ loginForm.addEventListener('submit', async (e) => {
   const email = document.getElementById('login-email').value;
   const password = document.getElementById('login-password').value;
 
-  loginError.classList.add('hide');
-  loginBtn.innerText = 'Logging in';
-  loginBtn.setAttribute('aria-busy', 'true');
+  loginError.classList.add('is-hidden');
+  loginBtn.classList.add('is-loading');
 
   try {
     await axios.post('/api/users/login', {
@@ -19,13 +18,12 @@ loginForm.addEventListener('submit', async (e) => {
     // Redirect to home
     window.location.href = '/';
   } catch (e) {
-    loginError.classList.remove('hide');
+    loginError.classList.remove('is-hidden');
     
     if (e.message) {
       loginError.innerText = e.message;
     }
   } finally {
-    loginBtn.innerText = 'Login';
-    loginBtn.setAttribute('aria-busy', 'false');
+    loginBtn.classList.remove('is-loading');
   }
 });

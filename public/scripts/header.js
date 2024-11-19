0591dd19ae562ea24c364e827534ada99b544010
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2024-11-19 10:08:38 +0545

    Change home style

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -2,10 +2,13 @@ const logoutBtn = document.getElementById('logout-btn');
 
 logoutBtn.addEventListener('click', async () => {
     try {
+        logoutBtn.classList.add('is-loading');
         await axios.post('/api/users/logout');
 
         window.location.href = '/login';
     } catch(e) {
         console.log(e);
+    } finally {
+        logoutBtn.classList.remove('is-loading');
     }
 });
